ecca06422e5ae2349db33cffd00e6ef731b06a14
robin thorsen <robinbthorsen@gmail.com>
2022-11-04 17:00:01 -1000

    changed title name

--- src/hello-world/script.py
+++ src/hello-world/script.py
@@ -2,7 +2,7 @@ import panel as pn
 pn.extension(sizing_mode="stretch_width", template="fast")
 pn.state.template.param.update(
     site="Panel Dashboards",
-    title="Hello World",
+    title="Crypto stock dashboard",
     favicon="https://raw.githubusercontent.com/firobeid/firobeid.github.io/main/docs/compose-plots/Resources/favicon.ico",
 )
 pn.panel(
