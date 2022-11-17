47498c9dccc8089f0952d2c97b935eecaf88b914
kaioff <kaiofadul@gmail.com>
2022-11-17 13:56:25 -0800

    depolying dash

--- questionary.py
+++ questionary.py
@@ -1,29 +1,18 @@
 import ccxt
 # jupyter lab --NotebookApp.iopub_data_rate_limit=1.0e10 - this command is required to run when opening jupyter labs or ccxt wont work in jupyter. or configure a config file.
 import pandas as pd
-import hvplot.pandas
-# from dotenv import load_dotenv
-import alpaca_trade_api as tradeapi
 import os
-import sqlalchemy as sql
+# import sqlalchemy as sql
 import sys
 import questionary
 from MCForecastTools import MCSimulation
 from warnings import filterwarnings
 filterwarnings("ignore")
-import ccxt
-# jupyter lab --NotebookApp.iopub_data_rate_limit=1.0e10 - this command is required to run when opening jupyter labs or ccxt wont work in jupyter. or configure a config file.
 import pandas as pd
 import hvplot.pandas
-# from dotenv import load_dotenv
-import alpaca_trade_api as tradeapi
-import os
-import sqlalchemy as sql
-import sys
 import numpy as np
 from MCForecastTools import MCSimulation
 from warnings import filterwarnings
-# import pandas_ta as ta
 from scipy.stats import norm
 import math
 import matplotlib.pyplot as plt
