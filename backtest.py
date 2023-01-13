1f3c9681c6465dd131827cb586e40c6575c58d7a
zache5 <zacheras@gmail.com>
2023-01-12 22:10:14 -0800

    pushing new code

--- backtest.py
+++ backtest.py
@@ -3,7 +3,8 @@
 
 # In[7]:
 
-
+import matplotlib
+matplotlib.use("TkAgg") 
 import streamlit as st
 from datetime import date
 import plotly.graph_objects as go
@@ -13,7 +14,8 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 from finta import TA
-
+import ta
+from talib import abstract
 
 # In[15]:
 
@@ -69,8 +71,10 @@ data = load_data(selected_stocks)
 
 
 # In[4]:
-
-
+st.subheader("Asset Data Head")
+st.write(data.head())
+st.subheader('Asset Data Tail')
+st.write(data.tail())
 st.subheader("Interactive Asset Chart")
 def plot_raw_data():
     fig = go.Figure()
@@ -96,41 +100,54 @@ strategy = (selected_strategy)
 
 def execute_trade(data, strategy):
     # Initialize indicators
-    rsi = TA.RSI(data['Adj Close'])
-    macd = TA.MACD(data['Adj Close'])
-    macd_values = macd.macd
-    signal_values = macd.signal
+    rsi = TA.RSI(data)
+    macd, macd_signal, macd_hist = abstract.MACD(data['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
+    macd_df = pd.DataFrame(macd, columns=['MACD'])
 
     # Initialize variables to keep track of trades
-    buys = []
-    sells = []
+    buy_indices = []
+    buy_closes = []
+    sell_indices = []
+    sell_closes = []
 
     # Iterate through the data and execute trades
     for i in range(1, len(data)):
         # RSI strategy
         if strategy == 'RSI':
             if rsi[i] < 30:
-                buys.append(i)
-            elif rsi[i] > 70:
-                sells.append(i)
+                buy_indices.append(i)
+                buy_closes.append(data.loc[i, 'Close'])
         
-        # MACD strategy
-        elif strategy == 'MACD':
-            if macd.macd[i] < macd.signal[i] and macd.macd[i-1] > macd.signal[i-1]:
-                buys.append(i)
-            elif macd.macd[i] > macd.signal[i] and macd.macd[i-1] < macd.signal[i-1]:
-                sells.append(i)
-                
-
-    # Plot the trades
+            elif rsi[i] > 70:
+                sell_indices.append(i)
+                sell_closes.append(data.loc[i, 'Close'])
+            # Plot RSI buys and sells 
     plt.plot(data['Close'], '-', label='Close Price')
-    plt.plot(rsi, '-', label='RSI')
-    plt.plot(macd.macd, '-', label='MACD')
-    plt.scatter(buys, data.loc[buys, 'Close'], color='green', marker='^',label='Buy')
-    plt.scatter(sells, data.loc[sells, 'Close'], color='red', marker='v',label='Sell')
+    # plt.plot(rsi, '-', label='RSI')
+    plt.scatter(buy_indices, buy_closes, color='green', marker='^', label='Buy')
+    plt.scatter(sell_indices, sell_closes, color='red', marker='v', label='Sell')
     plt.legend()
-    plt.show()
-    
+    plt.title('RSI Strategy')
+    st.pyplot()
+
+
+    #     elif strategy == 'MACD':
+    #         if macd[i] < macd_signal[i] and macd[i-1] > macd_signal[i-1]:
+    #             buy_dates.append(data.loc[i, 'Date'])
+    #             buy_closes.append(data.loc[i, 'Close'])
+    #         elif macd[i] > macd_signal[i] and macd[i-1] < macd_signal[i-1]:
+    #             sell_dates.append(data.loc[i, 'Date'])
+    #             sell_closes.append(data.loc[i, 'Close'])
+
+    #         # Plot MACD buys and sells
+    # plt.plot(data['Close'], '-', label='Close Price')
+    # plt.plot(macd_df['MACD'], '-', label='MACD')
+    # plt.scatter(buy_dates, buy_closes, color='green', marker='^', label='Buy')
+    # plt.scatter(sell_dates, sell_closes, color='red', marker='v', label='Sell')
+    # plt.legend()
+    # plt.title('MACD Strategy')
+    # plt.show()
+
 execute_trade(data,selected_strategy)
 
 
